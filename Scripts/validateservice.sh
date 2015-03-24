9d7aeea221948fc501e7ddeebdf568357416c3c0
Shaun Pearce <sppearce@10ddb1e9d13d.ant.amazon.com>
2015-03-24 19:09:59 +0000

    broken

--- Scripts/validateservice.sh
+++ Scripts/validateservice.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-exit 1
+RESULT=$(curl --silent localhost | grep V)
+
+if [ -z "$RESULT" ]; then
+	echo "Application not responding as expected. Failing."
+	exit 1
+fi
+echo "Application successfully responding as expected. Passing."
+exit 0
