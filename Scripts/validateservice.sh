00a5db0919e37769bf5ecc691537d4325ca17b94
Shaun Pearce <sppearce@10ddb1e9d13d.ant.amazon.com>
2015-04-13 11:32:21 +0100

    updated script again

--- Scripts/validateservice.sh
+++ Scripts/validateservice.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-RESULT=$(curl --silent localhost | grep V)
+RESULT=$(curl --silent localhost)
+VERSION=$(echo $RESULT | grep V)
 
-if [ -z "$RESULT" ]; then
+if [ -z "$VERSION" ]; then
 	echo "Application not responding as expected. Could not find version identifier in output: '$RESULT' Failing."
 	exit 1
 fi
